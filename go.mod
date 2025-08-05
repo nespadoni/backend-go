4354951472fd23a5d8cb9eee68a74beb788dfca4
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-08-05 09:32:03 -0400

    refactor(user): substitui tratamento de erros para uso do pacote goerror

--- go.mod
+++ go.mod
@@ -33,7 +33,7 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/nespadoni/goerror v0.0.0-20250805122127-52b36bdcc6bc // indirect
+	github.com/nespadoni/goerror v0.0.0-20250805131951-40e833852f91 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
 	github.com/rogpeppe/go-internal v1.14.1 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
--- internal/user/user-repository.go
+++ internal/user/user-repository.go
@@ -1,59 +1,60 @@
 package user
 
-//
-//import (
-//	"gorm.io/gorm"
-//)
-//
-//type UsuarioRepository struct {
-//	DB *gorm.DB
-//}
-//
-//func NovoRepositorioUsuario(db *gorm.DB) *UsuarioRepository {
-//	return &UsuarioRepository{DB: db}
-//}
-//
-//func (r *UsuarioRepository) BuscarTodos() []Usuario {
-//	var usuarios []Usuario
-//	resultado := r.DB.Find(&usuarios)
-//	handler.NewReadError("Erro ao buscar todos os usuários: ", resultado.Error)
-//	return usuarios
-//}
-//
-//func (r *UsuarioRepository) BuscarPorId(id int) Usuario {
-//	var usuario Usuario
-//	resultado := r.DB.Where("ID = ?", id).Find(&usuario)
-//	handler.NewReadError("Erro ao buscar usuário por ID", resultado.Error)
-//	return usuario
-//}
-//
-//func (r *UsuarioRepository) BuscarPorNome(nome string) Usuario {
-//	var usuario Usuario
-//	resultado := r.DB.Where("Nome = ?", nome).Find(&usuario)
-//	handler.NewReadError("Erro ao buscar usuário por nome", resultado.Error)
-//	return usuario
-//}
-//
-//func (r *UsuarioRepository) BuscarPorEmail(email string) Usuario {
-//	var usuario Usuario
-//	resultado := r.DB.Where("Email = ?", email).Find(&usuario)
-//	handler.Novo("Erro ao buscar usuário por email", resultado.Error)
-//	return usuario
-//}
-//
-//func (r *UsuarioRepository) SalvarUsuario(usuario Usuario) Usuario {
-//	resultado := r.DB.Create(&usuario)
-//	handler.NewCreateError("Erro ao salvar usuário: ", resultado.Error)
-//	return usuario
-//}
-//
-//func (r *UsuarioRepository) AtualizarUsuario(id int, novoUsuario Usuario) {
-//	resultado := r.DB.Where("id = ?", id).Updates(novoUsuario)
-//	handler.NewUpdateError("Erro ao atualizar usuário: ", resultado.Error)
-//}
-//
-//func (r *UsuarioRepository) DeletarUsuario(id int) {
-//	usuario := Usuario{}
-//	resultado := r.DB.Where("id = ?", id).Delete(&usuario)
-//	handler.NewDeleteError("Erro ao deletar usuário: ", resultado.Error)
-//}
+import (
+	"github.com/nespadoni/goerror"
+	"gorm.io/gorm"
+)
+
+type UsuarioRepository struct {
+	DB *gorm.DB
+}
+
+func NovoRepositorioUsuario(db *gorm.DB) *UsuarioRepository {
+	return &UsuarioRepository{DB: db}
+}
+
+func (r *UsuarioRepository) BuscarTodos() []Usuario {
+	var usuarios []Usuario
+	resultado := r.DB.Find(&usuarios)
+	goerror.EhErroBancoDados(resultado)
+	return usuarios
+}
+
+func (r *UsuarioRepository) BuscarPorId(id int) Usuario {
+	var usuario Usuario
+	resultado := r.DB.Where("ID = ?", id).Find(&usuario)
+	goerror.EhErroBancoDados(resultado)
+	return usuario
+}
+
+func (r *UsuarioRepository) BuscarPorNome(nome string) Usuario {
+	var usuario Usuario
+	resultado := r.DB.Where("Nome = ?", nome).Find(&usuario)
+	goerror.EhErroBancoDados(resultado)
+	return usuario
+}
+
+func (r *UsuarioRepository) BuscarPorEmail(email string) Usuario {
+	var usuario Usuario
+	resultado := r.DB.Where("Email = ?", email).Find(&usuario)
+	goerror.EhErroBancoDados(resultado)
+	return usuario
+}
+
+func (r *UsuarioRepository) SalvarUsuario(usuario Usuario) Usuario {
+	resultado := r.DB.Create(&usuario)
+	goerror.EhErroBancoDados(resultado)
+	return usuario
+}
+
+func (r *UsuarioRepository) AtualizarUsuario(id int, novoUsuario Usuario) {
+	resultado := r.DB.Where("id = ?", id).Updates(novoUsuario)
+	goerror.EhErroBancoDados(resultado)
+
+}
+
+func (r *UsuarioRepository) DeletarUsuario(id int) {
+	usuario := Usuario{}
+	resultado := r.DB.Where("id = ?", id).Delete(&usuario)
+	goerror.EhErroBancoDados(resultado)
+}
