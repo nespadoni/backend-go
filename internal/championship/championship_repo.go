3d09af4750876eff9baa53790187185786e9d6a9
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-08-22 09:12:20 -0400

    feat(championship): adiciona métodos FindById e Delete ao ChampionshipRepository
    Inclui novos métodos `FindById` e `Delete` no ChampionshipRepository, com suporte a validações, tratamento de erros e transações. Refatora nomes de variáveis para melhorar a legibilidade e manter consistência no estilo de código.

--- internal/championship/championship_repo.go
+++ internal/championship/championship_repo.go
@@ -16,16 +16,29 @@ func NewChampionshipRepository(db *gorm.DB) *ChampionshipRepository {
 	return &ChampionshipRepository{BaseRepository: repository.BaseRepository{DB: db}}
 }
 
-func (repo *ChampionshipRepository) FindAll() ([]models.Championship, error) {
+func (r *ChampionshipRepository) FindAll() ([]models.Championship, error) {
 	var championships []models.Championship
 
-	result := repo.DB.Find(&championships)
+	result := r.DB.Find(&championships)
 	if result.Error != nil {
 		// Retorna o erro quando houver problema na consulta
 		return nil, result.Error
 	}
 
 	return championships, nil
+}
+
+func (r *ChampionshipRepository) FindById(championship *models.Championship) (*models.Championship, error) {
+	var result models.Championship
+
+	if err := r.DB.First(&result, championship.ID).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("campeonato não encontrado")
+		}
+		return nil, fmt.Errorf("erro no banco de dados: %w", err)
+	}
+
+	return &result, nil
 }
 
 func (r *ChampionshipRepository) Create(championship *models.Championship) error {
@@ -52,5 +65,21 @@ func (r *ChampionshipRepository) Update(championship *models.Championship) error
 		}
 
 		return tx.Save(championship).Error
+	})
+}
+
+func (r *ChampionshipRepository) Delete(championship *models.Championship) error {
+	return r.WithTransaction(func(tx *gorm.DB) error {
+		var existing models.Championship
+
+		if err := tx.First(&existing, championship.ID).Error; err != nil {
+			if errors.Is(err, gorm.ErrRecordNotFound) {
+				return fmt.Errorf("registro não encontrado")
+			}
+
+			return fmt.Errorf("erro de banco: %w", err)
+		}
+
+		return tx.Delete(championship).Error
 	})
 }
