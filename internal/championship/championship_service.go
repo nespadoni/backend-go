64365a2831755340047bb78fd3f0404dc332af6f
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-08-26 11:21:16 -0400

    feat(championship): adiciona métodos FindAll, FindById e Create ao ChampionshipService
    Inclui novos métodos `FindAll`, `FindById` e `Create` no ChampionshipService, com validações, tratamento de erros e suporte à conversão de dados. Refatora assinatura de `FindChampionship` para `FindAll` e faz ajustes no construtor para incluir validação com `go-playground/validator`.

--- internal/championship/championship_service.go
+++ internal/championship/championship_service.go
@@ -1,21 +1,49 @@
 package championship
 
-import "backend-go/internal/models"
+import (
+	"backend-go/internal/models"
+	"fmt"
+
+	"github.com/go-playground/validator/v10"
+	"github.com/jinzhu/copier"
+)
 
 type Service struct {
-	repo Repository
+	repo     Repository
+	validate *validator.Validate
 }
 
 func NewChampionshipService(repo *Repository) *Service {
 	return &Service{repo: *repo}
 }
 
-func (service Service) FindChampionship() ([]models.Championship, error) {
+func (s Service) FindAll() ([]models.Championship, error) {
 
-	championship, err := service.repo.FindAll()
+	championship, err := s.repo.FindAll()
 	if err != nil {
 		return nil, err
 	}
 
 	return championship, nil
+}
+
+func (s Service) FindById(championshipId *models.Championship) (Response, error) {
+	championship, err := s.repo.FindById(championshipId)
+	if err != nil {
+		return Response{}, fmt.Errorf("erro no serviço de buscar usuário: %w", err)
+	}
+
+	var champResponse Response
+	if err := copier.Copy(&champResponse, championship); err != nil {
+		return Response{}, fmt.Errorf("erro ao buscar converter dados: %w", err)
+	}
+
+	return champResponse, nil
+}
+
+func (s Service) Create(championship *models.Championship) (Response, error) {
+	if err := s.validate.Struct(championship); err != nil {
+		return Response{}, fmt.Errorf("dados inválidos: %w", err)
+	}
+
 }
