3fbac623ad70f2f0d55e524d061671bb0229cf31
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-08-27 10:40:05 -0400

    feat(athletic): adiciona estrutura inicial para Requests e Responses
    Inclui os tipos `CreateRequest`, `UpdateRequest`, `UpdateStatusRequest`, `Response` e `ListResponse` para suportar funcionalidades relacionadas à entidade Athletic. Adiciona validações nos Requests utilizando `go-playground/validator` para garantir a consistência dos dados.

---
+++ internal/athletic/athletic_request.go
@@ -0,0 +1,25 @@
+package athletic
+
+type CreateRequest struct {
+	Name         string `validate:"required,min=3,max=100" json:"name"`
+	Description  string `validate:"required,min=10,max=1000" json:"description"`
+	Logo         string `validate:"omitempty,url,max=255" json:"logo,omitempty"`
+	CoverImage   string `validate:"omitempty,url,max=255" json:"cover_image,omitempty"`
+	IsPublic     bool   `json:"is_public,omitempty"`
+	UniversityId int    `validate:"required,min=1" json:"university_id"`
+	CreatorId    int    `validate:"required,min=1" json:"creator_id"`
+}
+
+type UpdateRequest struct {
+	Name        string `validate:"required,min=3,max=100" json:"name"`
+	Description string `validate:"required,min=10,max=1000" json:"description"`
+	Logo        string `validate:"omitempty,url,max=255" json:"logo,omitempty"`
+	CoverImage  string `validate:"omitempty,url,max=255" json:"cover_image,omitempty"`
+	IsActive    bool   `json:"is_active,omitempty"`
+	IsPublic    bool   `json:"is_public,omitempty"`
+}
+
+type UpdateStatusRequest struct {
+	IsActive bool `json:"is_active"`
+	IsPublic bool `json:"is_public"`
+}
---
+++ internal/athletic/athletic_response.go
@@ -0,0 +1,32 @@
+package athletic
+
+import (
+	"backend-go/internal/models"
+	"time"
+)
+
+type Response struct {
+	Id             int               `json:"id"`
+	Name           string            `json:"name"`
+	Description    string            `json:"description"`
+	Logo           string            `json:"logo,omitempty"`
+	CoverImage     string            `json:"cover_image,omitempty"`
+	IsActive       bool              `json:"is_active"`
+	IsPublic       bool              `json:"is_public"`
+	FollowersCount int               `json:"followers_count"`
+	University     models.University `json:"university"`
+	Creator        *models.User      `json:"creator,omitempty"`
+	CreatedAt      time.Time         `json:"created_at"`
+	UpdatedAt      time.Time         `json:"updated_at"`
+}
+
+type ListResponse struct {
+	Id             int               `json:"id"`
+	Name           string            `json:"name"`
+	Description    string            `json:"description"`
+	Logo           string            `json:"logo,omitempty"`
+	IsActive       bool              `json:"is_active"`
+	IsPublic       bool              `json:"is_public"`
+	FollowersCount int               `json:"followers_count"`
+	University     models.University `json:"university"`
+}
