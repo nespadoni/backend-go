0fe0847ee4ec66da1a99918c2eb5cc374564ff76
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-09-02 11:07:20 -0400

    refactor(user): altera tipo do campo `UniversityId` para `uint`
    Atualiza o tipo do campo `UniversityId` na struct `UserRequest` de `int` para `uint`, garantindo consistência com o padrão adotado para IDs em outros módulos.

--- internal/modules/user/user_request.go
+++ internal/modules/user/user_request.go
@@ -17,7 +17,7 @@ type UpdateUserRequest struct {
 	Name          string     `validate:"required,min=2,max=100" json:"name"`
 	Email         string     `validate:"required,email,max=100" json:"email"`
 	Telephone     string     `validate:"required,max=20" json:"telephone"`
-	UniversityId  *int       `json:"university_id,omitempty"`
+	UniversityId  *uint      `json:"university_id,omitempty"`
 	StudentStatus string     `validate:"required,oneof=active graduated viewer prospective" json:"student_status"`
 	DateOfBirth   *time.Time `json:"date_of_birth,omitempty"`
 	AthleticRole  *string    `validate:"omitempty,oneof=member director president" json:"athletic_role,omitempty"`
