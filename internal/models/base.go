3c5b925c94ba7a673d11defc25d63f6333d0299f
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-09-02 11:07:09 -0400

    refactor(models): altera tipo do campo `ID` de `int` para `uint`
    Atualiza o tipo do campo `ID` na struct `Base` para `uint`, alinhando com padrões de uso em outros módulos e garantindo maior consistência na manipulação de IDs.

--- internal/models/base.go
+++ internal/models/base.go
@@ -5,7 +5,7 @@ import (
 )
 
 type Base struct {
-	ID        int        `gorm:"primaryKey" json:"id"`
+	ID        uint       `gorm:"primaryKey" json:"id"`
 	CreatedAt time.Time  `json:"created_at"`
 	UpdatedAt *time.Time `json:"updated_at"`
 	DeletedAt *time.Time `json:"deleted_at"`
