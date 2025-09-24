bb17982c59d97e1442615522893f7a300abcb736
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-09-24 09:44:44 -0400

    add lineup repository

--- internal/modules/lineup/lineup_repository.go
+++ internal/modules/lineup/lineup_repository.go
@@ -1,1 +1,13 @@
 package lineup
+
+import (
+	"backend-go/internal/repository"
+)
+
+type Repository struct {
+	repository *repository.BaseRepository
+}
+
+func NewLineUpRepository(repository *repository.BaseRepository) *Repository {
+	return &Repository{repository: repository}
+}
