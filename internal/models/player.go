c23c9a86459f08ac732741eff704c0149c07c940
Neto Spadoni <137246816+nespadoni@users.noreply.github.com>
2025-09-24 09:24:01 -0400

    add new fields on player struct

--- internal/models/player.go
+++ internal/models/player.go
@@ -9,4 +9,11 @@ type Player struct {
 	Position   Position `gorm:"foreignKey:PositionID;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
 	UserID     *int     `json:"user_id"`
 	User       *User    `gorm:"foreignKey:UserID;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;"`
+
+	// Informações adicionais
+	Nickname string  `gorm:"size:50" json:"nickname"`
+	Height   float32 `json:"height"`
+	Weight   float32 `json:"weight"`
+	Status   string  `gorm:"size:100" json:"status"`
+	Notes    string  `gorm:"size:500" json:"notes"`
 }
